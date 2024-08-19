d88d121c427a9935a2c79e64b74524719cba863b
Gopikrishna P <141819210+techy-kriss-savvy@users.noreply.github.com>
2024-08-19 16:06:12 +0530

    Time: 239 ms (5.13%), Space: 45.6 MB (16.96%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int singleNumber(int[] nums)
+    {
+        int count = 0 , unique = 0;
+        for(int  i = 0 ; i < nums.length ; i++)
+        {
+                 count = 0;
+            for(int j = 0 ; j < nums.length ; j++)
+            {
+                        if(nums[i] == nums[j])
+                        {
+                            count++;
+                        }
+            }
+            if(count == 1)
+            {
+                unique = nums[i];
+            }
+        }
+        return unique;
+    }
+}
