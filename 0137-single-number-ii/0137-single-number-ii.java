23ba265b141d3a2df8f08c7db7126fab0b8eacce
Gopikrishna P <141819210+techy-kriss-savvy@users.noreply.github.com>
2024-08-19 16:20:44 +0530

    Time: 82 ms (5.13%), Space: 45.5 MB (35.72%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -2,6 +2,7 @@ class Solution {
     public int singleNumber(int[] nums)
     {
         int count = 0 , unique = 0;
+        Arrays.sort(nums);
         for(int  i = 0 ; i < nums.length ; i++)
         {
                  count = 0;
